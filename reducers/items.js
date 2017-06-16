5c68f964c703b860a9e1260d55eef59fdc9a5d29
Aaron Van Bokhoven <bokhoven@gmail.com>
2017-06-15 22:07:36 -1000

    Add item reducer
    Contains ADD_ITEM and REMOVE_ITEM actions

---
+++ reducers/items.js
@@ -0,0 +1,28 @@
+// Start the sequence of item ID's at 0
+let nextItemId = 0;
+
+// Items reducer
+const items = (state = [], action) => {
+  switch (action.type) {
+    case "ADD_ITEM": {
+      return [
+        ...state,
+        {
+          id: nextItemId++,
+          name: action.name,
+          bgColor: action.bgColor
+        }
+      ];
+    }
+    case "REMOVE_ITEM": {
+      // Find index of item with matching ID and then
+      // remove it from the array by its' index
+      const index = state.findIndex(x => x.id === action.id);
+      return [...state.slice(0, index), ...state.slice(index + 1)];
+    }
+    default:
+      return state;
+  }
+};
+
+export default items;
